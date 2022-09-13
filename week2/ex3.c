2dbba25f4e709b6f5a22adcab638029ce3c0a09d
Ikram Kamat <ikram.kamat.04@gmail.com>
2022-09-13 23:51:43 +0300

    fixed: fixed

--- week2/ex3.c
+++ week2/ex3.c
@@ -4,7 +4,7 @@
 const int MOD = 1e9 + 7;
 
 long long binpow(int a, int n){
-    if(n == 0) return 1;
+    if(n <= 0) return 1;
     if(n % 2 == 0){
         long long b = binpow(a, n / 2) % MOD;
         return b * b;
@@ -12,32 +12,16 @@ long long binpow(int a, int n){
     return binpow(a, n - 1) % MOD * a % MOD;
 }
 
-// void swap(char *x, char *y){
-//     char temp = *x;
-//     *x = *y;
-//     *y = temp;
-// }
-
-// void reverse(char* arr){
-//     int n = strlen(arr) - 1;
-//     for (int i = 0; i <= n/2; i ++){
-//         swap(arr + i, arr + (n - i));
-//     }
-// }
-
-void convert(char* from, int s, int t){
-    long long dec;
-    int indx = 0;
-    // reverse(from);
-    int n = strlen(from) - 1;
-    for (int i = 0; i <= n/2; i ++){
-        char temp = from[i];
-        from[i] = from[n - i];
-        from[n - i] = temp;
-    }
-    while (from[indx]){
-        dec += ((binpow(s, indx)) * (from[indx] - '0'));
-        indx ++;
+void convert(long long num, int s, int t){
+    long long dec = 0, pw = 1;
+    while(num){
+        if(num % 10 >= s){
+            printf("cannot convert");
+            return;
+        }
+        dec += (num % 10) * pw;
+        pw *= s;
+        num /= 10;
     }
     char to[33] = {};
     while (dec){
@@ -45,9 +29,7 @@ void convert(char* from, int s, int t){
         sprintf(to + strlen(to), "%d", x);
         dec /= t;
     }
-    // reverse(to);
-    // printf("%lu", strlen(to));
-    n = strlen(to) - 1;
+    int n = strlen(to) - 1;
     for (int i = 0; i <= n/2; i ++){
         char temp = to[i];
         to[i] = to[n - i];
@@ -57,9 +39,9 @@ void convert(char* from, int s, int t){
 }
 
 int main (){
-    char from[33];
+    long long from;
     int s, t;
-    scanf("%s", from);
+    scanf("%lld", &from);
     scanf("%d", &s);
     scanf("%d", &t);
     convert(from, s, t);    
